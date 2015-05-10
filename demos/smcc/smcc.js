773e6cbf04e51bc5191b34385c34ad9259385c46
tpphu <vietean@gmail.com>
2015-05-10 23:06:03 +0700

    - Update code

--- demos/smcc/smcc.js
+++ demos/smcc/smcc.js
@@ -16,16 +16,26 @@ window.onload = function () {
 	});
 	var move = function (dx, dy) {
 		console.log('----------------------------');
-		console.log(this.data('origTransform'));
-		console.log(this.data('origTransform') ? "T" : "t");
-		console.log([dx, dy]);
-		console.log('===>');
-		console.log(this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]);
+		var origPosition = this.data('origPosition');
+		console.log(origPosition);
+		var x = origPosition[0] + dx - 50;
+		var y = origPosition[1] + dy - 50;
+		var m = new Snap.Matrix();
+			m.translate(x, y);
+		this.attr({
+			//transform: this.transform().localMatrix.translate(x, y)
+			//transform: this.transform().globalMatrix.translate(dx, dy)
+			//transform: this.transform().diffMatrix.translate(dx, dy)
+			transform: m
+		});
+		return;
         this.attr({
 			transform: this.data('origTransform') + (this.data('origTransform') ? "T" : "t") + [dx, dy]
 		});
 	};
-	var start = function () {
+	var start = function (x, y, e) {
+		console.log('start arguments: ', arguments);
+		this.data('origPosition', [x, y]);
         this.data('origTransform', this.transform().local);
 	};
 	var stop = function () {
